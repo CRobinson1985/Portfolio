4bb65a2e1892729955735d03762cff31c123ec45
crobinson1985 <crobinson92828@gmail.com>
2025-06-22 12:36:02 -0400

    v0.22

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -1,15 +1,47 @@
+import { useState } from "react";
 import "./contact.css";
+
 export default function Contact() {
+
+  const [result, setResult] = React.useState("");
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    setResult("Sending....");
+    const formData = new FormData(event.target);
+
+    formData.append("access_key", "YOUR_ACCESS_KEY_HERE");
+
+    const response = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      body: formData
+    });
+
+    const data = await response.json();
+
+    if (data.success) {
+      setResult("Form Submitted Successfully");
+      event.target.reset();
+    } else {
+      console.log("Error", data);
+      setResult(data.message);
+    }
+  };
+
   return (
-    <div id="contact">
+    <div id="contact" className="contact-div">
       <h2>Contact me</h2>
       <p>Interested in employing my services or collaborating? Please feel free to get in touch! You can also get in touch with me through my social links below.</p>
       <form action="submit">
-        <input type="email" name="email" id="email-input" />
-        <input type="name" name="name" id="name-input" />
-        <input type="text" name="message" id="message-input" />
+        <label htmlFor="name-input">Name</label>
+        <input type="name" name="name" id="name-input" required />
+        <label htmlFor="email-input">Email</label>
+        <input type="email" name="email" id="email-input" required />
+        <label htmlFor="message-input">Enter a Quick Message</label>
+        <textarea name="message" id="message-input"></textarea>
         <button id="submit-btn" type="submit">Submit</button>
       </form>
+      <span>{result}</span>
     </div>
 
   );
--- src/components/services/services.jsx
+++ src/components/services/services.jsx
@@ -2,13 +2,13 @@ import "./services.css";
 
 export default function Services() {
   return (
-    <div id="services">
+    <div id="services" className="services-div">
       <h2>My Services</h2>
       <ul>
         <li>Custom Websites</li>
         <li>WordPress Websites</li>
         <li>Site Issues Fixed</li>
-        <li></li>
+        <li>Collaborations</li>
       </ul>
     </div>
 
