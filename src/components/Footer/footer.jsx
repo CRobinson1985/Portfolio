1c9ee5fe137f8cbc4273005587e1693140081c5a
crobinson1985 <crobinson92828@gmail.com>
2025-06-23 09:35:17 -0400

    v0.26 update: update social links. Remove facebook, add discord

--- src/components/Footer/footer.jsx
+++ src/components/Footer/footer.jsx
@@ -1,8 +1,8 @@
 import "./footer.css";
-import facebookSocial from "../../assets/pics/facebook.svg";
 import linkedinSocial from "../../assets/pics/linkedin.svg";
 import githubSocial from "../../assets/pics/github.svg";
 import twitchSocial from "../../assets/pics/twitch.svg";
+import discordSocial from "../../assets/pics/discord.svg";
 import emailSocial from "../../assets/pics/envelope.svg";
 
 export default function Footer () {
@@ -10,15 +10,7 @@ export default function Footer () {
     <footer>
       <p>Check me out Online!</p>
       <div>
-        <a className="social-card"
-          href="https://www.facebook.com/chris.robinson.147793"
-          target="_blank"
-          rel="noopener noreferrer"
-          ><img
-            className="social-icons"
-            src={facebookSocial}
-            alt="Facebook Logo"
-        /></a>
+        
         <a className="social-card"
           href="https://www.linkedin.com/in/christopherrobinsonwebdeveloper/"
           target="_blank"
@@ -28,6 +20,7 @@ export default function Footer () {
             src={linkedinSocial}
             alt="LinkedIn Logo"
         /></a>
+
         <a className="social-card"
           href="https://github.com/CRobinson1985"
           target="_blank"
@@ -37,6 +30,7 @@ export default function Footer () {
             src={githubSocial}
             alt="GitHub Logo"
         /></a>
+
         <a className="social-card"
           href="https://www.twitch.tv/mrbawkbagock"
           target="_blank"
@@ -45,6 +39,16 @@ export default function Footer () {
             className="social-icons"
             src={twitchSocial}
             alt="Twitch Logo"
+        /></a>
+
+        <a className="social-card"
+          href="https://discord.gg/drP6fhfXDS"
+          target="_blank"
+          rel="noopener noreferrer"
+          ><img
+            className="social-icons"
+            src={discordSocial}
+            alt="Discord Logo"
         /></a>
         
         <a className="social-card" href="mailto:crobinson92828@gmail.com">
