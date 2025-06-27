5ea0d33e97ec2fc69de37b3c1c779fdca4a28ab5
crobinson1985 <crobinson92828@gmail.com>
2025-06-27 16:44:43 -0400

    v0.28 Add: -Added a more substantive About Me section text.
    -Added more substantive information in my Services section.
    
    Update:
    -updated characteristics of the About Me div, blurring and darkening the background of the div.

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -4,22 +4,39 @@ import './about.css';
 export default function About() {
   return (
     <div id="about" className="about">
-      <div>
-        <img
-          id="my-portrait"
-          src={myself}
-          alt="A Picture of the creator of this website, Christopher"
-        />
-      </div>
-      <div className='about-text'>
-        <h2>About Me</h2>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut molestiae
-          aliquam culpa perspiciatis quibusdam, iure ex ullam, provident
-          corporis sapiente pariatur consequatur fugit possimus, neque enim
-          totam. Neque, quos explicabo.
-        </p>
-      </div>
+      <img
+        id="my-portrait"
+        src={myself}
+        alt="A Picture of the creator of this website, Christopher"
+      />
+
+      <h2>About Me</h2>
+      <p>
+        As a kid, I always had a love for computers. They blew my mind with how
+        much you could do with them. When I grew ino my mid teens, I taught
+        myself how to code in HTML with a guide to programming. I consumed it
+        and enjoyed every moment of it. Eventually, I also acquired a guide for
+        Visual Basic and C, and started the process learning them on my own.
+      </p>
+      <p>
+        My career life as a young adult had gone a different route from coding,
+        however. When I was young, I had figured I would need a degree to ever
+        get into coding, but the time to get a degree, as younger me, seemed too
+        far away. I wish I could have slapped my younger self, honestly! Life
+        took me into the realm of cars. I fell in love with them and wanted to
+        know the in's and outs of them. So, I became an automotive mechanic. I
+        fixed cars for a number years. Eventually, I found an opportunity to go
+        into Sales, and jumped at the chance. I've sold automotive service, then
+        brand new Automobiles, and finally mattresses.
+      </p>
+      <p>
+        Now, 25+ years after I initially found that love for coding, my passion
+        has been reignited. I have completed a number of great courses on
+        Full-stack Web Development, JavaScript, TypeScript, and Responsive Web
+        Design. My Journey has been a long and winding road. But it's
+        interesting how things can come back around eventually to where you are
+        meant to be. And, Coding is 100% where I am meant to be.
+      </p>
     </div>
   );
 }
--- src/components/services/services.jsx
+++ src/components/services/services.jsx
@@ -1,9 +1,14 @@
-import "./services.css";
+import './services.css';
 
 export default function Services() {
   return (
     <div id="services" className="services-div">
       <h2>My Services</h2>
+      <p>
+        I offer my services as a freelance Web Developer. I have been working
+        dilligently on conquering and mastering React. This website has been
+        coded and compiled with React.js. Though, I am happy to work with WordPress, Frame, or others
+      </p>
       <ul>
         <li>Custom Websites</li>
         <li>WordPress Websites</li>
@@ -11,6 +16,5 @@ export default function Services() {
         <li>Collaborations</li>
       </ul>
     </div>
-
   );
 }
