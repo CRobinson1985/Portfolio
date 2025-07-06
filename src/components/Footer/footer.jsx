9719e82fb3caa0e4a07e4b0aec3336d4c9722b26
crobinson1985 <crobinson92828@gmail.com>
2025-07-06 16:16:33 -0400

    v0.32 -- Added Facebook and instagram links. --Fixed sizing for social links.

--- src/components/Footer/footer.jsx
+++ src/components/Footer/footer.jsx
@@ -1,56 +1,84 @@
-import "./footer.css";
-import linkedinSocial from "../../assets/pics/linkedin.svg";
-import githubSocial from "../../assets/pics/github.svg";
-import twitchSocial from "../../assets/pics/twitch.svg";
-import discordSocial from "../../assets/pics/discord.svg";
-import emailSocial from "../../assets/pics/envelope.svg";
+import './footer.css';
+import linkedinSocial from '../../assets/pics/linkedin.svg';
+import githubSocial from '../../assets/pics/github.svg';
+import twitchSocial from '../../assets/pics/twitch.svg';
+import discordSocial from '../../assets/pics/discord.svg';
+import facebookSocial from '../../assets/pics/facebook.svg';
+import instagramSocial from '../../assets/pics/instagram.svg';
 
-export default function Footer () {
+export default function Footer() {
   return (
     <footer>
       <p>Check me out Online!</p>
       <div>
-        
-        <a className="social-card"
+        <a
+          className="social-card"
+          href="https://www.instagram.com/christhewebguy/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img
+            className="social-icons"
+            src={instagramSocial}
+            alt="Instagram Logo"
+          />
+        </a>
+        <a
+          className="social-card"
+          href="https://www.facebook.com/profile.php?id=61578262461583  "
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img
+            className="social-icons"
+            src={facebookSocial}
+            alt="Facebook Logo"
+          />
+        </a>
+
+        <a
+          className="social-card"
           href="https://www.linkedin.com/in/christopherrobinsonwebdeveloper/"
           target="_blank"
           rel="noopener noreferrer"
-          ><img
+        >
+          <img
             className="social-icons"
             src={linkedinSocial}
             alt="LinkedIn Logo"
-        /></a>
+          />
+        </a>
 
-        <a className="social-card"
+        <a
+          className="social-card"
           href="https://github.com/CRobinson1985"
           target="_blank"
           rel="noopener noreferrer"
-          ><img
-            className="social-icons"
-            src={githubSocial}
-            alt="GitHub Logo"
-        /></a>
+        >
+          <img className="social-icons" src={githubSocial} alt="GitHub Logo" />
+        </a>
 
-        <a className="social-card"
+        <a
+          className="social-card"
           href="https://www.twitch.tv/mrbawkbagock"
           target="_blank"
           rel="noopener noreferrer"
-          ><img
-            className="social-icons"
-            src={twitchSocial}
-            alt="Twitch Logo"
-        /></a>
+        >
+          <img className="social-icons" src={twitchSocial} alt="Twitch Logo" />
+        </a>
 
-        <a className="social-card"
+        <a
+          className="social-card"
           href="https://discord.gg/drP6fhfXDS"
           target="_blank"
           rel="noopener noreferrer"
-          ><img
+        >
+          <img
             className="social-icons"
             src={discordSocial}
             alt="Discord Logo"
-        /></a>
-        
+          />
+        </a>
       </div>
     </footer>
   );
