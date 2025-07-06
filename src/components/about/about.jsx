45b0393ed990622a754d5942cc4df9c1185ccb5e
crobinson1985 <crobinson92828@gmail.com>
2025-07-06 14:52:36 -0400

    v0.30 --minor fixes --Updated about me section. Reworded, and elaborated on some things.

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -12,30 +12,31 @@ export default function About() {
 
       <h2>About Me</h2>
       <p>
-        As a kid, I always had a love for computers. They blew my mind with how
-        much you could do with them. When I grew into my mid teens, I taught
-        myself how to code in HTML with just a HTML guide book. I consumed it
-        and enjoyed every moment of it. Eventually, I also acquired a guide for
-        Visual Basic and C, and started the process learning them on my own,
-        too.
+        As a kid, I'd always loved computers. They blew my mind, how much you
+        could do with them! From gaming to communicating with others, to
+        performing math and writing, it fascinated me. When I grew into my
+        teens, I took a deeper dive into computers by teaching myself how to
+        code in HTML with just an HTML guide book. I consumed it and enjoyed
+        every moment of it. Eventually, I also acquired a guide for Visual Basic
+        and C, and started the devouring them, as well.
       </p>
       <p>
         My career life as a young adult had gone a different route from coding,
         however. When I was young, I had figured I would need a degree to ever
         get into coding, but the time to get a degree, as younger me, seemed too
-        far away. I wish I could have slapped my younger self, honestly! Life
-        took me into the realm of cars. I fell in love with them and wanted to
-        know the in's and outs of them. So, I became an automotive mechanic. I
-        fixed cars for a number years. Eventually, I found an opportunity to go
-        into Sales, and jumped at the chance. I've sold automotive service, then
-        brand new Automobiles. Ultimately, ending my sales journey in mattress
-        sales.
+        far away. I wish I could have slapped my younger self, honestly! If only
+        I knew what I was capable of doing at the time! After that, life took me
+        into the realm of cars. I fell in love with them and wanted to know the
+        in's and outs of them. So, I became an automotive mechanic and fixed cars
+        for a number years. Eventually, I found an opportunity to go into sales,
+        and jumped at the chance. I've sold automotive service, then brand new
+        Automobiles. Ultimately, ending my sales journey in mattress sales.
       </p>
       <p>
         Now, 25+ years after I initially found that love for coding, my passion
         has been reignited. I have completed a number of great courses on
         Full-stack Web Development, JavaScript, TypeScript, and Responsive Web
-        Design. And, I have been working hard on some great real-world projects.
+        Design. I have also been working hard on some great real-world projects.
         One of my projects that I am most proud of being the first in my gallery
         for an amazing Italian and Biology teacher. My Journey has been a long
         and winding road. But it's interesting how things can come back around
