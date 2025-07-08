a78a2d95d1647c0d1757595b18158c2c83b405c9
crobinson1985 <crobinson92828@gmail.com>
2025-07-08 09:53:18 -0400

    v0.33 --Fixed h1 sizing and spacing. --Removed Bella Hotels and another project from the gallery. --Updated descriptions of gallery parts to be slightly more technical.

--- src/components/data.js
+++ src/components/data.js
@@ -22,72 +22,57 @@ const data = [
     name: "Love NYC",
     link: "https://nyc-love.vercel.app/",
     alt: "Preview of a Website for Lovers of New York City",
-    description: "I am currently working on making this project. In the end, I am working to make a really clean, modern looking site about one of the greatest cities on earth.",
+    description: "Compiled with Vite, React.js, and vanilla CSS styling. NYC is one of the greatest (if not THE) cities on earth.",
     img: NYCImg,
   }, 
   {
     name: "Simon Game",
     link: "https://crobinson1985.github.io/Simon-Game/",
     alt: "Preview of a Simon-style Game",
-    description: "This project was created through my Full-Stack Web Development course. It's a simple game of Simon Says. I think my best so far is level 12...",
+    description: "Compiled with Vanilla JavaScript and CSS styling. This was a project from my Full-Stack Certification.",
     img: simonImg
   },
   {
     name: "Digital Drums",
     link: "https://crobinson1985.github.io/Drum-Kit/",
     alt: "Preview of a digital drum set",
-    description: "From my Full-Stack Web Development course. This was a fun project! I have, on occassion, had a drum sesh or two, with this.",
+    description: "Compiled with Vanilla JavaScript and CSS Styling. This was also a project created from my Full-Stack Certification.",
     img: drumImg
   },
   {
     name: "Equillibrium Crypto Preview Card",
     link: "https://crobinson1985.github.io/nft-preview-card-component-main/",
     alt:  "Preview of a Crypto token website card",
-    description: "This is a project that I had created from Frontend Mentor. They have a great assortment of challenging coding projects for any level of coding knowledge.",
+    description: "Compiled with HTML and CSS Styling. This was a challenge project from Front-End Mentor",
     img: equilibriumImg
   },
   {
     name: "FAQ Card",
     link: "https://crobinson1985.github.io/faq-accordion-main/",
     alt: "Preview of a FAQ website",
-    description: "I have enjoyed many of the challenges from Frontend Mentor, and this was a great test of skills, for me.",
+    description: "Compiled with HTML, Vanilla JavaScript, and CSS styling. This was another Challenge project from Front-End Mentor.",
     img: faqImg
   },
   {
     name: "Interactive Rating Card",
     link: "https://crobinson1985.github.io/Rating-Demo-Project/",
     alt: "Preview of a Rating Score website card",
-    description: "Yet another project from Frontend Mentor. I can not say too much positive about them. Their projects are great practice, and help me realize my mistakes, and build my skills.",
+    description: "Compiled with HTML, Vanilla JavaScript, and CSS styling. Yet another Front-End Mentor project challenge.",
     img: ratingImg
   },
   {
     name: "Omelette Recipe Webpage",
     link: "https://crobinson1985.github.io/recipe-page-main/",
     alt: "Preview of an Omelette Recipe website",
-    description: "Frontend Mentor really doesn't hold your hand. All of your solutions are your own, and it is great for that.",
+    description: "Compiled with HTML and CSS styling. I've done a number of these challenge projects.",
     img: omeletteImg
   },
   {
     name: "Social Links Card",
     link: "https://crobinson1985.github.io/CRobinson.github.io-social-links-profile/",
     alt: "Preview of a Social link website card",
-    description: "This interactive Social Links card was really fun to make.",
+    description: "Compiled with HTML and CSS styling. Yea, this was another challenge project.",
     img: socialImg
-  },
-  {
-    name: "A Results Summary Component",
-    link: "https://crobinson1985.github.io/results-summary-component-main/",
-    alt: "Preview of a Results and Summary website card",
-    description: "This is a project I have completed for the website Frontend Mentor. It was a good experience, as were all of my projects, so far.",
-    img: summaryImg
-  },
-  {
-            
-    name: "Bella Hotels",
-    link: "https://crobinson1985.github.io/Hotel-Project/",
-    alt: "Preview of a Fictional Hotel Website",
-    description: "This was a project from my Full-Stack Web Development course as well, for a ficticious Hotel. I personally made the graphic for this site. I'm happy with how it came out!",
-    img: bellaImg
   },
   
 ];
