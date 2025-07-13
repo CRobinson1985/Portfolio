81c19dd05250a635d4fa357115cdd0a39c63a177
crobinson1985 <crobinson92828@gmail.com>
2025-07-13 00:33:58 -0400

    v0.34 --Added the Italian Destination Wedding Co website to my gallery section

--- src/components/data.js
+++ src/components/data.js
@@ -1,78 +1,98 @@
-import valImg from "../assets/pics/vals-site-hero.jpg";
-import simonImg from "../assets/pics/Simon.jpg";
-import drumImg from "../assets/pics/Drums.jpg";
-import equilibriumImg from "../assets/pics/equillibrium.jpg";
-import faqImg from "../assets/pics/faq.jpg";
-import ratingImg from "../assets/pics/rating.jpg";
-import omeletteImg from "../assets/pics/omelette.jpg";
-import socialImg from "../assets/pics/social-links.jpg";
-import NYCImg from "../assets/pics/Experience NYC.jpg"
+import valImg from '../assets/pics/vals-site-hero.jpg';
+import simonImg from '../assets/pics/Simon.jpg';
+import drumImg from '../assets/pics/Drums.jpg';
+import equilibriumImg from '../assets/pics/equillibrium.jpg';
+import faqImg from '../assets/pics/faq.jpg';
+import ratingImg from '../assets/pics/rating.jpg';
+import omeletteImg from '../assets/pics/omelette.jpg';
+import socialImg from '../assets/pics/social-links.jpg';
+import NYCImg from '../assets/pics/Experience NYC.jpg';
+import tidwcImg from '../assets/pics/tidwc.jpg';
 
 const data = [
   {
-    name: "Benvenuto(a) su LearnWithVal",
-    link: "https://crobinson1985.github.io/LearnWithVal/",
+    name: 'Benvenuto(a) su LearnWithVal',
+    link: 'https://crobinson1985.github.io/LearnWithVal/',
     alt: "Preview of a Language Teacher's website",
-    description: "Please feel free to check out this website that I've created for the Best Italian language teacher. If you are curious to learn Italian with an amazing teacher, please reach out to her!",
+    description:
+      "Please feel free to check out this website that I've created for the Best Italian language teacher. If you are curious to learn Italian with an amazing teacher, please reach out to her!",
     img: valImg,
-  }, 
+  },
+
   {
-    name: "Love NYC",
-    link: "https://nyc-love.vercel.app/",
-    alt: "Preview of a Website for Lovers of New York City",
-    description: "Compiled with Vite, React.js, and vanilla CSS styling. NYC is one of the greatest (if not THE) cities on earth.",
+    name: 'The Italian Destination Wedding Co',
+    link: 'https://italian-wedding-co.vercel.app/',
+    alt: 'Preview of The Italian Destination Wedding Co Website',
+    description:
+      'This is a Current project of mine for a wedding destination company. They specialize in helping people set up their dream wedding in Italy.',
+    img: tidwcImg,
+  },
+
+  {
+    name: 'Love NYC',
+    link: 'https://nyc-love.vercel.app/',
+    alt: 'Preview of a Website for Lovers of New York City',
+    description:
+      'Compiled with Vite, React.js, and vanilla CSS styling. NYC is one of the greatest (if not THE) cities on earth.',
     img: NYCImg,
-  }, 
+  },
+
   {
-    name: "Simon Game",
-    link: "https://crobinson1985.github.io/Simon-Game/",
-    alt: "Preview of a Simon-style Game",
-    description: "Compiled with Vanilla JavaScript and CSS styling. This was a project from my Full-Stack Certification.",
-    img: simonImg
+    name: 'Simon Game',
+    link: 'https://crobinson1985.github.io/Simon-Game/',
+    alt: 'Preview of a Simon-style Game',
+    description:
+      'Compiled with Vanilla JavaScript and CSS styling. This was a project from my Full-Stack Certification.',
+    img: simonImg,
   },
   {
-    name: "Digital Drums",
-    link: "https://crobinson1985.github.io/Drum-Kit/",
-    alt: "Preview of a digital drum set",
-    description: "Compiled with Vanilla JavaScript and CSS Styling. This was also a project created from my Full-Stack Certification.",
-    img: drumImg
+    name: 'Digital Drums',
+    link: 'https://crobinson1985.github.io/Drum-Kit/',
+    alt: 'Preview of a digital drum set',
+    description:
+      'Compiled with Vanilla JavaScript and CSS Styling. This was also a project created from my Full-Stack Certification.',
+    img: drumImg,
   },
   {
-    name: "Equillibrium Crypto Preview Card",
-    link: "https://crobinson1985.github.io/nft-preview-card-component-main/",
-    alt:  "Preview of a Crypto token website card",
-    description: "Compiled with HTML and CSS Styling. This was a challenge project from Front-End Mentor",
-    img: equilibriumImg
+    name: 'Equillibrium Crypto Preview Card',
+    link: 'https://crobinson1985.github.io/nft-preview-card-component-main/',
+    alt: 'Preview of a Crypto token website card',
+    description:
+      'Compiled with HTML and CSS Styling. This was a challenge project from Front-End Mentor',
+    img: equilibriumImg,
   },
   {
-    name: "FAQ Card",
-    link: "https://crobinson1985.github.io/faq-accordion-main/",
-    alt: "Preview of a FAQ website",
-    description: "Compiled with HTML, Vanilla JavaScript, and CSS styling. This was another Challenge project from Front-End Mentor.",
-    img: faqImg
+    name: 'FAQ Card',
+    link: 'https://crobinson1985.github.io/faq-accordion-main/',
+    alt: 'Preview of a FAQ website',
+    description:
+      'Compiled with HTML, Vanilla JavaScript, and CSS styling. This was another Challenge project from Front-End Mentor.',
+    img: faqImg,
   },
   {
-    name: "Interactive Rating Card",
-    link: "https://crobinson1985.github.io/Rating-Demo-Project/",
-    alt: "Preview of a Rating Score website card",
-    description: "Compiled with HTML, Vanilla JavaScript, and CSS styling. Yet another Front-End Mentor project challenge.",
-    img: ratingImg
+    name: 'Interactive Rating Card',
+    link: 'https://crobinson1985.github.io/Rating-Demo-Project/',
+    alt: 'Preview of a Rating Score website card',
+    description:
+      'Compiled with HTML, Vanilla JavaScript, and CSS styling. Yet another Front-End Mentor project challenge.',
+    img: ratingImg,
   },
   {
-    name: "Omelette Recipe Webpage",
-    link: "https://crobinson1985.github.io/recipe-page-main/",
-    alt: "Preview of an Omelette Recipe website",
-    description: "Compiled with HTML and CSS styling. I've done a number of these challenge projects.",
-    img: omeletteImg
+    name: 'Omelette Recipe Webpage',
+    link: 'https://crobinson1985.github.io/recipe-page-main/',
+    alt: 'Preview of an Omelette Recipe website',
+    description:
+      "Compiled with HTML and CSS styling. I've done a number of these challenge projects.",
+    img: omeletteImg,
   },
   {
-    name: "Social Links Card",
-    link: "https://crobinson1985.github.io/CRobinson.github.io-social-links-profile/",
-    alt: "Preview of a Social link website card",
-    description: "Compiled with HTML and CSS styling. Yea, this was another challenge project.",
-    img: socialImg
+    name: 'Social Links Card',
+    link: 'https://crobinson1985.github.io/CRobinson.github.io-social-links-profile/',
+    alt: 'Preview of a Social link website card',
+    description:
+      'Compiled with HTML and CSS styling. Yea, this was another challenge project.',
+    img: socialImg,
   },
-  
 ];
 
-      export default data;
+export default data;
