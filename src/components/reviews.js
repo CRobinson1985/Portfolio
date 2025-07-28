bdc618d4ba6908680ca024edba95a47bc35c78ad
crobinson1985 <crobinson92828@gmail.com>
2025-07-28 09:45:18 -0400

    v0.371 --Added Valentina to review name

--- src/components/reviews.js
+++ src/components/reviews.js
@@ -5,7 +5,7 @@ const reviews = [
   //     'Probably one of the best! Very patient listens to your vision and offers great solutions. I highly recommend',
   // },
   {
-    name: 'Caffé e Particelle',
+    name: 'Valentina Caffé e Particelle',
     review:
       "Christopher created my italian website! It's awesome! He is professional, down to earth, polite, accurate and passionate about his job. He is always precise and concerns about the client's needs. I definitely recommend his services for your company, personal website or any other needs. This guy deserves the gold! Hard work pays off!",
   },
